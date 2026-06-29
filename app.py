d3c04c13683c8b96479bdf2b1b70fe1495c83d49
rahat1517 <gmrashidulislam003@gmail.com>
2026-06-29 16:57:17 +0600

    hide streamlit activity

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ if not os.path.exists(FILE_NAME):
     df.to_csv(FILE_NAME, index=False)
 
 
-st.set_page_config(page_title="স্মার্ট মানি ম্যানেজার", layout="centered")
+st.set_page_config(page_title="স্মার্ট মানি ম্যানেজার", layout="centered",menu_items={
+    'Get Help': None',
+    'Report a bug': None,
+    'About' :None
+})
 st.title("💰 আমার স্মার্ট মানি ম্যানেজার")
 st.write("সহজে খরচ ট্র্যাক করুন এবং হিসেব রাখুন।")
 
